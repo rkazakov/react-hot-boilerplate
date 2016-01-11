3dd74e6330c8f47960d7ad41f7a5665e21149764
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-01-11 16:50:31 +1100

    Adding MeterialUI components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Checkbox from 'material-ui/lib/checkbox';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Hello, world.</h1>
+        <RaisedButton label="Default" />
+        <Checkbox />
+      </div>
     );
   }
 }
